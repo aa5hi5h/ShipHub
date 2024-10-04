bc619543f99ab07a6a7083cf88db65132a2608c3
aashish <quakie.exe@gmail.com>
2024-10-04 18:17:22 +0530

    monorepo initialized

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import { add } from "@foundation/sample-libs"
+
+export default function Home() {
+  return (
+    <div className="text-4xl">de6aa5hi5h {add(3424,423234)}</div>
+  );
+}
